0ad4d9cc9da6782eda80bca8210702db8f23ab73
Your Name <you@example.com>
2023-07-31 05:49:35 +0000

    テスト例を追加

--- test_main.sh
+++ test_main.sh
@@ -1,29 +1,59 @@
 #!/usr/bin/bash
 
+# いろいろな工夫を紹介するので力量に併せてテストを行おう
+
+# ------------------------------
+# ファイル名を変数にして入力省略
+ans="/tmp/$$-ans"
+result="/tmp/$$-result"
+err_log="/tmp/$$-errors"
 
 
-# 入力数が少ない場合
-# 入力なし
-# 入力1個
-# 入力3個以上
+# ------------------------------
+# 中断する類のエラーが起きた時の処理
+function err_catch {
+        echo "エラー時の処理"
+}
+trap catch err_catch
 
-# 正の自然数以外が入力された場合
-# a b -> "error: plz input natural number
-# -1
+# 成功・失敗に関わらず行いたい処理
+function final_func {
+	rm ${ans} ${result} ${err_log}
+}
+trap finally final_func
 
+
+# ------------------------------
+# エラー動作時のチェック
+## 正しいエラーメッセージか確認するため
+## 標準エラー出力の内容を確認する
+# エラー時に出力してほしい正解データ
+echo "Input 2 argments" > ${ans}
+# エラー時のデータ（入力数が足りない）
+./main.sh 2> ${result} # 正しいエラーメッセージか確認するので2
+diff ${ans} ${result} || echo "test 1-1: no input" >> ${err_log}
+
+
+# ------------------------------
+# 正常動作時のチェック
 # 最大公約数が計算できているか
-# 2 4 -> 2
+# 正常動作の正解データ
 echo "2" > /tmp/$$-ans
+# 正常動作の出力
 ./main.sh 2 4 > /tmp/$$-result
-diff /tmp/$$-ans /tmp/$$-result || echo "テスト3-1" >> /tmp/$$-error.log
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト2-1" >> /tmp/$$-error.log
 
 
-# 1000 100 -> 100
-# 12 8 -> 4
-
+# ------------------------------
+# エラーメッセージを出力する
 if [ -f /tmp/$$-error.log ];then
         cat /tmp/$$-error.log 1>&2
         rm -f /tmp/$$-*
         exit 1
 fi
 
+
+# ここで挙げたものは一例なので、
+# こういう時にエラーが起きそう
+# というものを確認できるテストしよう
+# 例：負の数、少数、極端に大きい数字、文字
